7b3153b409eee206865dd508318525d64d029d42
David Pitty <dpitty13@gmail.com>
2018-04-14 07:30:17 -0600

    separated windows from mac commands

--- process.sh
+++ process.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 grep -ve '^#' pre-programming_dict.json > programming_dict.json
+grep -ve '^#' pre-windows.json > windows.json
+grep -ve '^#' pre-mac.json > mac.json
+grep -ve '^#' pre-linux.json > linux.json
